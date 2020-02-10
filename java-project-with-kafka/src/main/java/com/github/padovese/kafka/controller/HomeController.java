bd1efce35284001bc0d7b7ca502d320ef328562c
padovese <bhspadoveze@gmail.com>
2020-02-09 22:02:50 -0300

    feat: producer with metadata and key

--- java-project-with-kafka/src/main/java/com/github/padovese/kafka/controller/HomeController.java
+++ java-project-with-kafka/src/main/java/com/github/padovese/kafka/controller/HomeController.java
@@ -1,8 +1,10 @@
 package com.github.padovese.kafka.controller;
 
 import com.github.padovese.kafka.config.KafkaConfig;
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,13 +26,61 @@ public class HomeController {
     private final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     @GetMapping("/{prefix}/{rep}")
-    public String home(@PathVariable(name = "prefix") String prefix, @PathVariable(name = "rep") int rep) throws InterruptedException {
+    public String simpleProducer(@PathVariable(name = "prefix") String prefix, @PathVariable(name = "rep") int rep) throws InterruptedException {
 
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(kafkaConfig.properties());
 
         for(int rept = rep; rept > 0; rept--){
 
             ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", prefix + rept);
+            producer.send(record);
+            producer.flush();
+            logger.info(prefix + rept, "ok");
+        }
+
+        producer.close();
+
+        return "ok";
+    }
+
+    @GetMapping("/metadata/{prefix}/{rep}")
+    public String simpleProducerWithMetadata(@PathVariable(name = "prefix") String prefix, @PathVariable(name = "rep") int rep) throws InterruptedException {
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(kafkaConfig.properties());
+
+        for(int rept = rep; rept > 0; rept--){
+
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", prefix + rept);
+            //wait a callback with metadata
+            producer.send(record, (metadata, exception) -> {
+                if(exception == null){
+                    logger.info("Received. Topic: " + metadata.topic()
+                            + " - Partition: " + metadata.partition()
+                            + " - Offset: " + metadata.offset()
+                            + " - Timestamp: " + metadata.timestamp()
+                    );
+                } else{
+                    logger.error("Error while processing" + exception);
+                }
+            });
+            producer.flush();
+            logger.info(prefix + rept, "ok");
+        }
+
+        producer.close();
+
+        return "ok";
+    }
+
+    @GetMapping("/key/{prefix}/{rep}")
+    public String simpleProducerWithKey(@PathVariable(name = "prefix") String prefix, @PathVariable(name = "rep") int rep) throws InterruptedException {
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(kafkaConfig.properties());
+
+        for(int rept = rep; rept > 0; rept--){
+
+            //defining a key is sending a msg to the same partition, always. thus you can guarantee order
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "id_" + rept, prefix + rept);
             producer.send(record);
             producer.flush();
             logger.info(prefix + rept, "ok");
